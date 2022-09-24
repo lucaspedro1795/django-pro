98d8eee24e73fe304caf1b58958800d1131d2838
Pedro Lucas <lmorato553@gmail.com>
2022-09-24 20:34:19 -0300

    Alterando permissão para servidores.

--- base_paraiso/settings.py
+++ base_paraiso/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qkd4i!&^1dtp2g3mdzxc1x()xov#jyknyo$uvs4(c)rq&o0umb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
